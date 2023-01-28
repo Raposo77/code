4ed827557fe25b4d184452da00ac74b8b33d9db0
Ricardo Ferrari <ricjferrari@gmail.com>
2023-01-28 18:02:10 -0300

    atualizacao area do usuario

--- apps/dashboard/views.py
+++ apps/dashboard/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpRequest, HttpResponse
 from .forms import AvaliacaoForm, AlertaDeCriseForm, AnotacaoForm, ContatoAjuda
+from .models import Avaliacao
 from django.contrib.auth.decorators import login_required
 
 def index(request):
@@ -8,10 +9,18 @@ def index(request):
 
 @login_required(login_url='account:login')
 def dashboard(request: HttpRequest) -> HttpResponse:
+    avaliacoes = Avaliacao.objects.all()
     form_avaliacao = AvaliacaoForm()
     form_anotacao = AnotacaoForm()
-    
+
     return render(request, 'dashboard/dashboard.html', context={
+        'avaliacoes': avaliacoes,
         'form_avaliacao': form_avaliacao,
         'form_anotacao':  form_anotacao,
         })
+
+def crise(request):
+    return render(request, 'dashboard/crise.html')
+
+def ajuda(request):
+    return render(request, 'dashboard/ajuda.html')
