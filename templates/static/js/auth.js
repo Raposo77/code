9ae10d4de790af77b0aa2b72230d1bdabfbb0b6b
Ricardo Ferrari <ricjferrari@gmail.com>
2023-01-28 14:12:58 -0300

    configuracao frontend

---
+++ templates/static/js/auth.js
@@ -0,0 +1,15 @@
+document.querySelectorAll('.icon-eye').forEach(icon => {
+    icon.addEventListener('click', el => {
+        let classes = el.target.getAttribute('class').split(' ')
+        let password_input = el.target.parentElement.parentElement.querySelector('input[name*="password"]')
+        console.log(el.target.parentElement.parentElement);
+        if (classes.indexOf('fa-eye') == true) {
+            password_input.setAttribute('type', 'text')
+            classes[classes.indexOf('fa-eye')] = 'fa-eye-slash'
+        } else {
+            password_input.setAttribute('type', 'password')
+            classes[classes.indexOf('fa-eye-slash')] = 'fa-eye'
+        }
+        el.target.setAttribute('class', classes.join(' '))
+    })
+})
